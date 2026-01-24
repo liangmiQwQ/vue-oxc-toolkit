fb7fc5428648eecddfee64c46cece0b5d804c186
Liang Mi <github@liangmi.dev>
2026-01-24 10:10:19 +0800

    perf: use append as possible

--- crates/vue_oxc_toolkit/src/parser/modules.rs
+++ crates/vue_oxc_toolkit/src/parser/modules.rs
@@ -11,30 +11,30 @@ pub trait Merge: Sized {
 }
 
 impl Merge for ModuleRecord<'_> {
-  fn merge(&mut self, instance: Self) {
+  fn merge(&mut self, mut instance: Self) {
     self.has_module_syntax |= instance.has_module_syntax;
     self.requested_modules.extend(instance.requested_modules);
-    self.import_entries.extend(instance.import_entries);
+    self.import_entries.append(&mut instance.import_entries);
     self
       .local_export_entries
-      .extend(instance.local_export_entries);
+      .append(&mut instance.local_export_entries);
     self
       .indirect_export_entries
-      .extend(instance.indirect_export_entries);
+      .append(&mut instance.indirect_export_entries);
     self
       .star_export_entries
-      .extend(instance.star_export_entries);
+      .append(&mut instance.star_export_entries);
     self.exported_bindings.extend(instance.exported_bindings);
-    self.dynamic_imports.extend(instance.dynamic_imports);
-    self.import_metas.extend(instance.import_metas);
+    self.dynamic_imports.append(&mut instance.dynamic_imports);
+    self.import_metas.append(&mut instance.import_metas);
   }
 
-  fn merge_imports(&mut self, instance: Self) {
+  fn merge_imports(&mut self, mut instance: Self) {
     self.has_module_syntax |= instance.has_module_syntax;
     self.requested_modules.extend(instance.requested_modules);
-    self.import_entries.extend(instance.import_entries);
-    self.dynamic_imports.extend(instance.dynamic_imports);
-    self.import_metas.extend(instance.import_metas);
+    self.import_entries.append(&mut instance.import_entries);
+    self.dynamic_imports.append(&mut instance.dynamic_imports);
+    self.import_metas.append(&mut instance.import_metas);
   }
 }
 
