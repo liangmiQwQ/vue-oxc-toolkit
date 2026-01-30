23bb1b1a9f658b1d4aef638b44e33bb5cc37348f
Liang Mi <github@liangmi.dev>
2026-01-30 22:11:26 +0800

    test:

--- crates/vue_oxc_toolkit/fixtures/directive/v-slot.vue
+++ crates/vue_oxc_toolkit/fixtures/directive/v-slot.vue
@@ -1,14 +1,20 @@
 <template>
-  <Comp #="{ message }" />
+  <Comp #="{ message }"></Comp>
   <Comp #header="{ message }">
     {{ message }}
   </Comp>
-  <Comp v-slot="{ message }" />
+  <Comp v-slot></Comp>
+  <Comp v-slot />
   <Comp v-slot:header="{ message }">
     {{ message }}
   </Comp>
-  <Comp v-slot:="{ message }" />
+  <Comp v-slot:="{ message }">
+    {{ message }}
+  </Comp>
   <Comp v-slot:[key]="{ message }">
     {{ message }}
+  </Comp>
+  <Comp v-slot:header="user">
+    {{ user.name }}
   </Comp>
 </template>
