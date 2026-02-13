0ef919bb2f6b400e07086e15beebc9bd6e846140
Liang Mi <github@liangmi.dev>
2026-02-13 16:19:40 +0800

    benchmark

--- benchmark/benches/parse.rs
+++ benchmark/benches/parse.rs
@@ -1,13 +1,16 @@
+use std::hint::black_box;
+
 use criterion::{Criterion, criterion_group, criterion_main};
+use oxc_allocator::Allocator;
+use vue_oxc_toolkit::VueOxcParser;
 
 fn bench(c: &mut Criterion) {
   const VUE_SOURCE: &str = include_str!("../vue.vue");
 
   c.bench_function("parse_vue", |b| {
     b.iter(|| {
-      let x = 1;
-      let y = 2;
-      x + y
+      let allocator = Allocator::new();
+      black_box(VueOxcParser::new(&allocator, VUE_SOURCE).parse());
     });
   });
 }
